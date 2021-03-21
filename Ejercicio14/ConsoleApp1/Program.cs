ad703c5a9813ea12b05515409930bbc8b72622ae
diego.taub <diego.taub@accenture.com>
2021-03-21 20:59:05 -0300

    Ejercicio14

---
+++ Ejercicio14/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la primera frase: ");
+            string frase = Console.ReadLine();
+            Console.Write("Ingrese la segunda frase: ");
+            if(Console.ReadLine().ToUpper() == frase.ToUpper())
+            {
+                Console.Write("Las frases son iguales.");
+            }
+            else
+            {
+                Console.Write("Las frases son diferentes.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
